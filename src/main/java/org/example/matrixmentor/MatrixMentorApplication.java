cfa59138c80a3bee723049d3bfd66ea1d5d5d35b
yagutrzazada <yaqutrzazade@gmail.com>
2025-03-27 16:59:38 +0400

    first commit

---
+++ src/main/java/org/example/matrixmentor/MatrixMentorApplication.java
@@ -0,0 +1,13 @@
+package org.example.matrixmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixMentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatrixMentorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/matrixmentor/MatrixMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.matrixmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixMentorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
